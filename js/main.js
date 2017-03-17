0420869faf89c6669df9bebea1e8f2281d5ed0e6
Aurora-Boreas <borga78@list.ru>
2017-03-17 23:20:25 +0400

    Разметка на флексах

--- js/main.js
+++ js/main.js
@@ -16,3 +16,41 @@ toggle_btn.addEventListener("click", function(event) {
 });
 
 
+/*modal form*/
+var modal_open = document.querySelector(".btn--features-order");
+var product_buy = document.querySelectorAll(".btn--buy");
+
+var modal = document.querySelector(".modal-cart");
+var modal_overlay = document.querySelector(".modal-cart__overlay");
+
+
+if (modal_open) {
+    modal_open.addEventListener("click", function(event) {
+    event.preventDefault();
+    modal.classList.add("modal-cart--show");
+  });
+};
+
+if (modal_overlay) {
+modal_overlay.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal.classList.remove("modal-cart--show");
+});
+};
+
+for (var i = 0; i < product_buy.length; i++) {
+    product_buy[i].addEventListener("click", function(event) {
+        event.preventDefault();
+        if (modal) {
+            modal.classList.add("modal-cart--show");
+        }
+    });
+}
+
+/*frame*/
+var frame = document.querySelector(".contacts__iframe");
+var image = document.querySelector(".contacts__img");
+if (frame) {
+  frame.classList.remove("contacts__iframe--hidden");
+  image.classList.add("contacts__img--hidden");
+}
